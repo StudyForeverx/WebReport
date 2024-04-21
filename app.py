7b28fc2f2c8e7df467f2ca3073c8e3c73c7ada6c
liulesen <lesen4210001687@outlook.com>
2024-04-21 12:21:46 +0800

    2

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def phoneus():
 
 from flask import Flask, request, jsonify
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST'])  #login 测试
 def login():
     data = json.loads(request.get_data(as_text=True))  # 更简单的方式来解析 JSON 数据
     print(data)
@@ -25,11 +25,16 @@ def login():
     else:
         return jsonify({'success': False, 'message': 'Invalid credentials'}), 200  # 返回错误信息和401状态码
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST'])  #register 测试
 def register():
     data = json.loads(request.get_data(as_text=True)) # 更简单的方式来解析 JSON 数据
     print(data)
     return jsonify({'success': True}), 200
+
+# @app.route('/hongkong', methods=['GET'])
+# def gohongkong():
+#     return render_template('HongKong.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
