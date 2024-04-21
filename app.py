48a8f03aa3d33caada9da2a701f8811a9562513b
liaojiongyi <2750768449@qq.com>
2024-04-21 12:23:23 +0800

    数据库操作

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import json
 
 from flask import Flask, request, jsonify, render_template
 
+from controller.UserServerController import UserServerController
+
 app = Flask(__name__)
 
 # 处理根路径请求，返回欢迎页面
 @app.route('/', methods=['GET'])
 def index():
+
     return render_template('index.html')
 
 @app.route('/phoneus', methods=['GET'])
@@ -20,6 +23,8 @@ from flask import Flask, request, jsonify
 def login():
     data = json.loads(request.get_data(as_text=True))  # 更简单的方式来解析 JSON 数据
     print(data)
+    con = UserServerController()
+    result = con.findloginServerStatus(data)
     if str(data['username']) == '1111' and str(data['password']) == '2222':
         return jsonify({'success': True}), 200  # 返回 JSON 响应，并显示操作成功
     else:
@@ -29,10 +34,13 @@ def login():
 def register():
     data = json.loads(request.get_data(as_text=True)) # 更简单的方式来解析 JSON 数据
     print(data)
+    con = UserServerController()
+    result = con.adduserServerStatus(data)
     return jsonify({'success': True}), 200
 @app.route('/guangzhou', methods=['GET'])
 def goguangzhou():
     return render_template('guangzhou.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ controller/UserServerController.py
@@ -0,0 +1,43 @@
+import sys
+import time
+
+
+sys.path.append("../")
+
+from model.CommonDb import *
+import datetime
+from frameworks.utils.ApiUtil import *
+
+class UserServerController:
+    def __init__(self):
+        pass
+
+    def adduserServerStatus(self, status_data):
+        model = CommonDb('user')
+        consequence = model.selectAll(f"username='{status_data['username']}' and password='{status_data['password']}'")
+        if consequence:
+            result = False
+            print('已有此用户，无法添加')
+        else:
+            result = model.add(status_data)
+        return result
+
+    def findloginServerStatus(self, status_data):
+        model = CommonDb('user')
+        consequence = model.selectAll(f"username='{status_data['username']}' and password='{status_data['password']}'")
+        if not consequence:
+            result = False
+            print('无此项')
+        else:
+            result = True
+        return result
+
+    def orderhotelServerStatus(self, status_data):
+        model = CommonDb('orderinfo')
+        consequence = model.selectAll(f"name='{status_data['name']}' and phone='{status_data['phone']}' and IDCard='{status_data['IDCard']}'")
+        if consequence:
+            result = False
+            print('已有此用户，无法添加')
+        else:
+            result = model.add(status_data)
+        return result
